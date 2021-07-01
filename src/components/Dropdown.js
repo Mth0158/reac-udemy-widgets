68dfbc2ba5e2b772c251dd63cdc9092123f3cfb6
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-07-01 11:28:49 +0200

    Starting the translate widget

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
+    };
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -22,13 +39,20 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form" style={{ marginBottom: 40 }}>
+    <div ref={ref} className="ui form" style={{ marginBottom: 40 }}>
       <div className="field">
         <label className="label">Select a color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
